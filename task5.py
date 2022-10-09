a2dd4ed4984a9bfc023dbd1aa4f2b922e665e157
mila <milavoronina1997@mail.ru>
2022-10-10 00:01:17 +0500

    Task5 finally solved

--- task5.py
+++ task5.py
@@ -1,62 +1,78 @@
 # Задайте двумерный массив из целых чисел. Количество строк и столбцов задается с клавиатуры. 
 # Отсортировать элементы по возрастанию слева направо и сверху вниз.
 
-# from random import randint
-# n = int(input('Введите количество строк массива: '))
-# m = int(input('Введите количество столбцов массива: '))
-# result = [[randint(1, 100) for j in range(m)] for i in range(n)]
-# def printMatrix ( matrix ): 
-#    for i in range ( len(matrix) ): 
-#       for j in range ( len(matrix[i]) ): 
-#           print ( "{:4d}".format(matrix[i][j]), end = "" ) 
-#       print ()
-# printMatrix(result)
-# temp = 0
-# for i in len(result):
-#     for j in i:
-#         for k in j-1:
-#             if result[i][k] < result[i][k+1]:
-#                 temp = result[i][k+1]
-#                 result[i][k+1] = result[i][k]
-#                 result[i][k] = temp
-# printMatrix(result)
 
-size = int(input('size'))
-size2 = int(input('size'))
+# result = [[randint(1, 100) for j in range(m)] for i in range(n)]
+
+from random import randint
 def create_array(size,size2):
     array = []
     for i in range(size):
         buff = []
         for j in range(size2):
-            value = int(input('value'))
+            value =  randint(1, 100)  #int(input('value'))
             buff.append(value)
         array.append(buff)
     return array
 
-matrix = create_array(size,size2)
 
 def printMatrix (matrix): 
    for i in range (len(matrix) ): 
       for j in range ( len(matrix[i]) ): 
           print ( "{:4d}".format(matrix[i][j]), end = "" ) 
       print ()
-printMatrix(matrix)
 
 def sort_matrix(array):
-    min = array[0][0]
-    temp = 0
-    for i in range (len(array)):
-        for j in range(len(array[i])):
-            if min < array[i][j]:
-                temp = min
-                min = array[i][j]
-                array[i][j] = temp
-                return array
+    for i in range(len(array)- 1):
+        for j in range(len(array)-i-1):
+            if array[j] > array[j + 1]:
+                buff = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = buff
+try:
+
+    size = int(input('Введите количество строк массива: '))
+    size2 = int(input('Введите количество столбцов массива: '))
+    matrix = create_array(size,size2)
+    printMatrix(matrix)
+    x = len(matrix[0])#столбцы
+    y = len(matrix)#строки
+
+    table = []
+    for i in range(y):
+        for j in matrix[i]:
+            table.append(j)
+
+    sort_matrix(table)
+
+    count = size2
+    result = []
+    start = 0
+    end = count
+    while end <= len(table):
+        temp = []
+        for i in range(start, end):
+            temp.append(table[i])
+        result.append(temp)
+        start += count
+        end += count
+    print('Отсортированный массив:')
+    printMatrix(result)
+except:
+    print('Введите, пожалуйста, целые числа.')
+
+
+
+
+
+           
+
+
+
 
     
 
-result = sort_matrix(matrix)
-printMatrix(result)
+
                 
                 
 
